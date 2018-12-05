f8432d7845dbd551136e5f91794f57130b819156
admin <admin@MBP13-2010.Dlink>
2018-12-05 16:02:19 +0200

    Updates

---
+++ precache-manifest.b9795486cd621b4cd8426ee0766a7c9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5195b7ef53babb3d234a",
+    "url": "/dairyReact/static/js/runtime~main.5195b7ef.js"
+  },
+  {
+    "revision": "18f9cb21071bf694bb32",
+    "url": "/dairyReact/static/js/main.18f9cb21.chunk.js"
+  },
+  {
+    "revision": "bef273fe684dda98d4b3",
+    "url": "/dairyReact/static/js/1.bef273fe.chunk.js"
+  },
+  {
+    "revision": "18f9cb21071bf694bb32",
+    "url": "/dairyReact/static/css/main.2453b6d8.chunk.css"
+  },
+  {
+    "revision": "1282970b4e2623d4e3cbba57dd535c8e",
+    "url": "/dairyReact/index.html"
+  }
+];
